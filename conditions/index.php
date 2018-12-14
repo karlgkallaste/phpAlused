3217748e4790a2de40aa6ed1329923f1179aba90
karlgkallaste <karl-gustav.kallaste@khk.ee>
2018-12-14 12:52:27 +0200

    if ja Else if

--- conditions/index.php
+++ conditions/index.php
@@ -6,3 +6,32 @@
  * Time: 12:32
  */
 //tingimused
+/*
+ * if(tingimus){
+ * tegevused mis toimivad siis
+ * kui tingimus kehtib
+ * else{
+ * muidu kui tingimus ei kehti toimuvad siia
+ * kirjutatud tegevused
+ */
+echo '<h5>Ülesanne</h5>';
+$arv = 68;
+IF($arv %  2 == 0){
+    echo '<div style="color: green">'.$arv.'</div>';
+ }
+ else{
+    echo '<div style="color: red">'.$arv.'</div>';
+
+}
+echo'<hr>';
+echo '<h5>Ülesanne 2</h5>';
+$arv = rand(0,100);
+if($arv >= 0 and  $arv < 25) {
+    echo '<div style="color:red">' . $arv . '</div>';
+} else if ($arv >= 25 and $arv < 50) {
+    echo '<div style="color:green">' . $arv . '</div>';
+}else if ($arv >= 50 and $arv < 75) {
+    echo '<div style="color:blue">' . $arv . '</div>';
+}else if ($arv >= 75 and $arv < 100) {
+    echo '<div style="color:orange">' . $arv . '</div>';
+}
