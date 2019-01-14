38a63ad818567dad23a2f3f75e22274cf5cfb060
karlgkallaste <karl-gustav.kallaste@khk.ee>
2019-01-14 12:32:07 +0200

    Lisatud toote $_GET toodetele (1-3)

--- form/tellimine.php
+++ form/tellimine.php
@@ -1,3 +1,4 @@
+<h1>Tellimine</h1>
 <?php
 /**
  * Created by PhpStorm.
@@ -5,4 +6,13 @@
  * Date: 14.01.2019
  * Time: 12:23
  */
-print('Hello World!<br>');
+//lisab vormist saadud andmed muutujasse
+$toode1 = $_GET['t1'];
+$toode2 = $_GET['t2'];
+$toode3 = $_GET['t3'];
+
+echo 'Toode 1: '.$toode1.'tk<br>';
+echo 'Toode 2: '.$toode2.'tk<br>';
+echo 'Toode 3: '.$toode3.'tk<br>';
+?>
+
