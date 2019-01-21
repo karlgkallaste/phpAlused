ed8b418df1a0503f9ad02634d410cef526b46e48
karlgkallaste <karl-gustav.kallaste@khk.ee>
2019-01-21 13:09:37 +0200

    Loodud massiiv mis näitab tänast päeva.

--- date/index.php
+++ date/index.php
@@ -5,3 +5,13 @@
  * Date: 21.01.2019
  * Time: 12:09
  */
+echo "Ülesanne 8<br>";
+echo date('d.m.Y G:i' , time());
+echo "<br>Päev:";
+//kuude massiiv
+$p2ev = array(1=>'esmaspäev', 'teisipäev', 'kolmapäev', 'neljapäev', 'reede');
+//kuupäevad massiividesse
+$paev = $p2ev[date('n')];
+//kuupäeva väljastamine
+echo $paev;
+
