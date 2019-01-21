355c54e388fa47d9975b3f5d07f89c6fbde5ca03
karlgkallaste <karl-gustav.kallaste@khk.ee>
2019-01-21 13:37:23 +0200

    Vanuse osa, minu vanuse arvutamine ja vanuste vahe-..

--- date/index.php
+++ date/index.php
@@ -28,4 +28,31 @@ echo "<br>";
 $d1=strtotime("June 21");
 $d2=ceil(($d1-time())/60/60/24);
 echo "Järgmise Jaanipäevani on " . $d2 ." päeva";
+echo "<br>";
 //sünnipeäva arvutamine.
+$userDob = '1987-04-02';
+$userDob2 = '1999-05-31';
+
+//date time infost mis on yleval.
+$dob = new DateTime($userDob);
+$dob2 = new DateTime($userDob2);
+
+//v6rdlemine hetke kuup2eva ja synnip2evaga
+$now = new DateTime();
+$now2 = new DateTime();
+
+//vahe synnip2eval ja hetke kuup2eval
+$difference = $now->diff($dob);
+$difference2 = $now2->diff($dob2);
+
+//tema vanus, minu vanus ja vahe.
+$age = $difference->y;
+$age2 = $difference2->y;
+$vahe = $age - $age2;
+
+//Print osa
+echo "Esimese inimese vanus: " . $age. "aastat";
+echo "<br>";
+echo "Minu vanus: ". $age2. "aastat";
+echo "<br>";
+echo "Meie vanuse vahe: ".$vahe. "aastat";
