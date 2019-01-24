356576f87b3309281696b5046b919e37df609b7f
karlgkallaste <karl-gustav.kallaste@khk.ee>
2019-01-24 13:50:37 +0200

    lisatud select func

--- DB/index.php
+++ DB/index.php
@@ -8,5 +8,17 @@
 require_once ('db_fnk.php');
 require_once 'conf.php';
 $iktconn = connect_db(DBHOST, DBUSER,DBPASS,DBNAME);
+/*echo '<pre>';
+print_r($iktconn);*/
+//insert
+$sql = 'UPDATE user SET last_name="Ebatavaline" WHERE user_id= 1';
+$res = query($sql, $iktconn);
+
+//select
+$sql = 'SELECT * FROM user';
+$res = getdata($sql,$iktconn);
+
 echo '<pre>';
-print_r($iktconn);
+print_r($res);
+echo 'Tere '.$res[0]['first_name'];
+
